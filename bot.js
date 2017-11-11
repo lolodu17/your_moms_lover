91708f0d1c0efa932356cc1451033089057069be
BXD <bakuganxd@gmail.com>
2017-11-11 20:11:51 +0200

    Fix

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var chat = require('./chat_commands.js');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -12,38 +13,34 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on('ready', function () {
+    logger.info(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels.`);
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            case 'huy':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'pizda'
-                });
-            case 'pizda':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'huy'
-                });
-            break;
-            // Just add any case commands if you want to..
+     if (message.substring(0, 1) == '!') {
+            var args = message.substring(1).split(' ');
+            var cmd = args[0];
+           
+            args = args.splice(1);
+            switch(cmd) {
+                // !ping
+                case 'ping':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Pong!'
+                    });
+                case 'huy':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'pizda'
+                    });
+                case 'pizda':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'huy'
+                    });
+                break;
+                // Just add any case commands if you want to..
+             }
          }
-     }
 });
