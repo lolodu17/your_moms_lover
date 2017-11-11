c39d491d6bde4a8ddb0376e1fcc208a90dd4e33a
BXD <bakuganxd@gmail.com>
2017-11-12 00:32:13 +0200

    Version 1.0.1

--- bot.js
+++ bot.js
@@ -1,63 +1,57 @@
-var Discord = require('discord.io');
-var logger = require('winston');
+var Discord = require('discord.js');
 var auth = require('./auth.json');
 var prefix = '!';
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+//инициализация бота
+var bot = new Discord.Client({disableEveryone: true});
+
+bot.on('ready', async () => {
+    console.log(`Bot has started, with ${bot.users.size - 1} users, in ${bot.channels.size} channels.`);
+    //кэтч ошибок (стандарт ёпта)
+    try {
+        let link = await bot.generateInvite(["ADMINISTRATOR"]);
+        console.log(link);
+    } catch(e) {
+        console.log(e.stack);
+    }
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (channelID) {
-    logger.info(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels.`);
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+bot.on('message', async message => {
     //массив пентамемов
     var AnekdotArr = ['Лупа и Пупа устроились на работу. Проработали целый месяц, трудились не покладая рук и не жалея живота своего. В конце месяца Лупа и Пупа пошли получать зарплату. В бухгалтерии все как обычно перепутали. И, в итоге, Лупа получил за Пупу, а Пупа за ЛУПУ!', 'Сидят четыре пацана на скамейке, смотрят - Димон идёт. Один из пацанов говорит: -С кем Димон первый поздоровается - тот пид@р. Все согласились. Подходит к ним Димон и говорит: -Привет пацаны!', 'Сидят мужики в баре. Один говорит: - Сейчас приду домой и разорву трусики жены! - У вас что, до сих пор такие страстные отношения? - Нет, просто натерли очень!'];
     var rand = Math.floor(Math.random() * AnekdotArr.length);
     var getAnekdot = AnekdotArr[rand];
     //функции чат-бота
-        if (message.substring(0, 1) == prefix) {
-            var args = message.substring(1).split(' ');
-            var cmd = args[0];
-           
-            args = args.splice(1);
-            switch(cmd) {
+        if(message.author.bot) return;
+        if(message.content.indexOf(prefix) !== 0) return;
+        //отделение символа длиной в префикс а затем неведомая хуйня
+        let messageArray = message.content.slice(prefix.length).trim().split(/ !/g);
+        let command = messageArray.shift().toLowerCase();
+            //свич(нинтендо) на команды чата
+            switch(command) {
+                //тест эмбедов (хз чё это)
+                case 'userinfo':
+                    let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username)
+                        .setDescription("He is so dumb, LOL!");
+                    message.channel.send(embed);
+                    break;
                 case 'ping':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Pong!'
-                    });
+                    message.channel.send('Pong!');
                     break;
                 case 'huy':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'pizda'
-                    });
+                    message.channel.send('pizda');
                     break;
                 case 'pizda':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'huy'
-                    });
+                    message.channel.send('huy');
                     break;
                 case 'Za':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'lupa' 
-                    });   
+                    message.channel.send('lupa');   
                     break; 
                 case 'anekdot':
-                	bot.sendMessage({
-                		to: channelID,
-                		message: getAnekdot
-                	});
+                	message.channel.send(getAnekdot);
                     break;
              }
-         }
+        
 });
+//логин
+bot.login(auth.token);
